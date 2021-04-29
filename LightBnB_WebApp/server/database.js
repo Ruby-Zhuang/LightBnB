955fd668977a05abed419b0a7cc457529c67cb91
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-28 21:43:50 -0400

    Update addProperty function to save a new property to the properties table.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -11,28 +11,16 @@ const pool = new Pool({
   database: 'lightbnb'
 });
 
-// const queryString = `
-//   SELECT * FROM properties LIMIT $1;
-// `;
-
-// const values = [];
-
-// // Run query on database
-// return pool
-//   .query(queryString, values)
-//   .then((result) => result.rows) // Will return result.rows to next .then in apiRoutes file
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
-
-/// Users
+/////////////////////////////////////
+// Users ----------------------------
+/////////////////////////////////////
 
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
+const getUserWithEmail = function (email) {
   const queryString = `SELECT * FROM users WHERE email = $1;`; // Query string
   const queryParams = [email]; // Values from user
 
@@ -65,7 +53,6 @@ const getUserWithId = function (id) {
 };
 exports.getUserWithId = getUserWithId;
 
-
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
@@ -88,14 +75,16 @@ const addUser = function (user) {
 };
 exports.addUser = addUser;
 
-/// Reservations
+/////////////////////////////////////
+// Reservations ---------------------
+/////////////////////////////////////
 
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
+const getAllReservations = function (guest_id, limit = 10) {
   const queryString = `
     SELECT properties.*, reservations.*, avg(rating) as average_rating
     FROM reservations
@@ -119,7 +108,9 @@ const getAllReservations = function(guest_id, limit = 10) {
 };
 exports.getAllReservations = getAllReservations;
 
-/// Properties
+/////////////////////////////////////
+// Properties -----------------------
+/////////////////////////////////////
 
 /**
  * Get all properties.
@@ -127,7 +118,7 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
+const getAllProperties = function (options, limit = 10) {
   // 1
   const queryParams = [];
   // 2
@@ -175,10 +166,9 @@ const getAllProperties = function(options, limit = 10) {
     LIMIT $${queryParams.length};
   `;
 
-  // 5
-  console.log(queryString, queryParams);
+  // console.log(queryString, queryParams);
 
-  // 6
+  // 5
   return pool.query(queryString, queryParams)
     .then((result) => result.rows);
 };
@@ -190,10 +180,23 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+const addProperty = function(property) {
+  const queryString = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;
+  `;
+  // const queryParams = [...Object.values(property)]; // Values from user
+  const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]; // Values from user
+
+  // Run query on database
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    }) // Return property object CHECK THIS!!!!!!!!!!!!!!! should it be rows[0]?
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
 exports.addProperty = addProperty;
